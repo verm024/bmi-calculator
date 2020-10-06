b71ff42810bd23ae4383892370c7dfd47e6aa93e
verm024 <muhnaufal251@gmail.com>
2020-10-07 01:19:41 +0700

    add bmi category

--- api/index.js
+++ api/index.js
@@ -20,15 +20,40 @@ app.post('/calculate', (req, res, next) => {
     let height = Number(req.body.height)
     let weight = Number(req.body.weight)
     if(req.body.height == null || req.body.weight == null){
-        return res.status(400).send({error: 'No empty parameter allowed'})
+        return res.status(400).send({message: 'No empty parameter allowed'})
     }
     height = height / 100
     let bmi = weight / (height * height)
     if(bmi){
-        return res.status(200).send({bmi: bmi})
+        let category
+        if(bmi <= 16){
+            category = 'Severe Thinness'
+        }
+        else if(bmi <= 17){
+            category = 'Moderate Thinness'
+        }
+        else if(bmi <= 18.5){
+            category = 'Mild Thinness'
+        }
+        else if(bmi <= 25){
+            category = 'Normal'
+        }
+        else if(bmi <= 30){
+            category = 'Overweight'
+        }
+        else if(bmi < 35){
+            category = 'Obese Class I'
+        }
+        else if(bmi < 40){
+            category = 'Obese Class II'
+        }
+        else{
+            category = 'Obese Class III'
+        }
+        return res.status(200).send({bmi: bmi, category: category})
     }
     else{
-        return res.status(400).send({error: 'weight and height must be a number'})
+        return res.status(400).send({message: 'weight and height must be a number'})
     }
 })
 
